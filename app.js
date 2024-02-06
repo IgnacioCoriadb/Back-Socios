fedbe19767e6ec53656240e0fa6f8e8e53b44400
Ignacio <ignaciocoriadb@gmail.com>
2024-02-06 10:04:43 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(PORT, ()=>{
+    console.log('APP Running on port ' + PORT);
+})
