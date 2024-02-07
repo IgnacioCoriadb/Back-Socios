748a85f6a3298c6bd37ea62208c15545ab594543
Ignacio <ignaciocoriadb@gmail.com>
2024-02-07 09:55:03 -0300

    model person

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const sequelize = require('./config/postgreSql'); 
+const Person = require('./app/models/Person');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
---
+++ app/models/Person.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../../config/postgreSql');
+
+
+const Person = sequelize.define('Person',{
+    userName:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastName:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    dateOfBirth:{
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    age:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    address:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    phone :{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    DNI:{
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+
+
+})
